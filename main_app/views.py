dd2272e3638acddb3c541bb15d5146b3b494aaa3
Peter <pdnguyen714@gmail.com>
2020-04-28 17:19:34 -0700

    Added detail page refactored model to Django method

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
+from .models import Game
 
 from django.http import HttpResponse
 
-class Game:  # Note that parens are optional if not inheriting from another class
-  def __init__(self, name, company, description):
-    self.name = name
-    self.company = company
-    self.description = description
+# class Game:  # Note that parens are optional if not inheriting from another class
+#   def __init__(self, name, company, description):
+#     self.name = name
+#     self.company = company
+#     self.description = description
 
-games = [
-  Game('League Of Legends', 'Riot Games', 'Mobile Online Battle Arena that pins two teams of five against each other.'),
-  Game('Call Of Duty Warzone', 'Activision Blizzard', 'First Person Shooter that pins up to 100+ players against each other in different modes of play.'),
-  Game('Mobile Legends', 'Moonton', 'Mobile Online Battle Arena that pins two teams together - identical to LoL.')
-]
+# games = [
+#   Game('League Of Legends', 'Riot Games', 'Mobile Online Battle Arena that pins two teams of five against each other.'),
+#   Game('Call Of Duty Warzone', 'Activision Blizzard', 'First Person Shooter that pins up to 100+ players against each other in different modes of play.'),
+#   Game('Mobile Legends', 'Moonton', 'Mobile Online Battle Arena that pins two teams together - identical to LoL.')
+# ]
 
 def home(request):
     return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -21,4 +22,9 @@ def about(request):
     return render(request, 'about.html')
 
 def games_index(request):
+    games = Game.objects.all()
     return render(request, 'games/index.html', { 'games': games })
+
+def games_detail(request, game_id):
+    game = Game.objects.get(id=game_id)
+    return render(request, 'games/detail.html', { 'game': game})
