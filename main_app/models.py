48cef21f4f353fdd50d79da461d53b7ec9305bce
Peter <pdnguyen714@gmail.com>
2020-04-29 17:20:16 -0700

    Added Edit and Delete

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Game(models.Model):
     name = models.CharField(max_length=100)
@@ -7,3 +8,6 @@ class Game(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('games_detail', kwargs={ 'game_id': self.id })
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Game
 
 def home(request):
@@ -8,14 +9,27 @@ def home(request):
 def about(request):
     return render(request, 'about.html')
 
-def games_index(request):
-    games = Game.objects.all()
-    return render(request, 'games/index.html', { 'games': games })
+class GameList(ListView):
+    model = Game
+    
+    def get_queryset(self):
+        return Game.objects.all()
+
+class GameDetail(DetailView):
+    model = Game
 
 def games_detail(request, game_id):
     game = Game.objects.get(id=game_id)
-    return render(request, 'games/detail.html', { 'game': game})
+    return render(request, 'games/detail.html', {'game': game})
 
 class GameCreate(CreateView):
     model = Game
     fields = '__all__'
+
+class GameUpdate(UpdateView):
+    model = Game
+    fields = ['name', 'description']
+
+class GameDelete(DeleteView):
+    model = Game
+    success_url = '/games/'
