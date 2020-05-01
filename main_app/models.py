0cf943fbb96f9e271efaea08c85cbeaa66148b49
Peter <pdnguyen714@gmail.com>
2020-04-30 18:41:05 -0700

    Added Supported model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+SUPPORTED = (
+    ('P', 'Playstation'),
+    ('N', 'Nintendo'),
+    ('X', 'Xbox')
+)
+
 class Game(models.Model):
     name = models.CharField(max_length=100)
     company = models.CharField(max_length=100)
@@ -10,4 +16,20 @@ class Game(models.Model):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('games_detail', kwargs={ 'game_id': self.id })
+        return reverse('games_detail', kwargs={ 'pk': self.id })
+
+class Supported(models.Model):
+    date = models.DateField('feeding date')
+    support = models.CharField(
+        max_length=1,
+        choices=SUPPORTED,
+        default=SUPPORTED[0][0]
+    )
+
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_support_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
